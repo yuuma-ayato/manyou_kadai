069dbdce42b4d5d0670c34197d0c4a4e5dad54e3
akihiro tokuhara <muscletown_mcm@yahoo.co.jp>
2020-07-02 11:55:16 +0900

    終了期限、進捗、優先順位のバリデーション追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,9 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 50 }
   validates :content, presence: true, length: { maximum: 255 }
+  validates :limit, presence: true
+  validates :status, presence: true
+  validates :priority, presence: true
 
   scope :title_search,  -> (title) { where("title LIKE ?", "%#{title}%") }
   scope :status_search,  -> (status) { where(status: status)}
